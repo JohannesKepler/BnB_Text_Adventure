e293613fcc7b89877b39ad06f660eb1444308181
JohannesKepler <kwatzkwatz@gmail.com>
2014-07-14 17:16:14 -0400

    Update adventure.py
    More work on auto-map generation. Game now auto-generates a map by creating a seed room and then randomly appending the rest of the rooms onto a random choice of the existing rooms.

--- adventure.py
+++ adventure.py
@@ -33,13 +33,16 @@ def vector_to_dir(p, q):
         return None
 
 # dictionary of commands, displayed with help call
-helpful = {'go': "Moves you around. 'go north' or 'move north' are acceptable.",
-'look': "See where you can go based on room layout.",
-'x': "Examine something - type a word after x for something specific,\n or just x for a list of what is around you.",
-'take': "Pick something up if it is near you.",
-'i': "Check the items you are holding (inv).",
-'map': "Get a summary of the rooms you've visited.",
-'use': "You may need to use the things you find down here..."}
+helpful = {
+    'go': "Moves you around. Use 'look' to see where you can go.",
+    'look': "See where you can go based on room layout.",
+    'x': "Examine something - type a word after x for something specific,\n or just x for a list of what is around you.",
+    'take': "Pick something up if it is near you.",
+    'i': "Check the items you are holding.",
+    'map': "Get a summary of the rooms you've visited.",
+    'whereami': "Check where you are.",
+    'use': "You may need to use the things you find down here..."
+}
 
 class Map:
     # pull from list of random rooms
@@ -50,7 +53,12 @@ class Map:
     # does the map need to do anything, or is it static once created?
     def __init__(self):
         self.room_set = set()
-        self.populate()
+        self.room_set_pos = []
+        room_name = room_names.pop()
+        # create seed room
+        seed_room = Room(room_name, 0, 0)
+        self.room_set.add(seed_room)
+        self.room_set_pos.append([0, 0])
 
     # populate the map with a few rooms.
     # Rooms must be adjacent with at least one other,
@@ -60,34 +68,71 @@ class Map:
     # self.room_set = {} # goes in __init__ method
     # room_name = room_names.pop()
     # self.room_set[room_name] = Room(room_name)
-    def populate(self):
+    def build_map(self):
         while len(room_names) > 0:
             room_name = room_names.pop()
-            new_room = Room(room_name)
+            while True:
+                attach_to = random.sample(self.room_set, 1)[0]
+                if attach_to.check_neighbors:
+                    break
+            attach_to_pos = attach_to.get_pos()
+            offset_x = random.choice([-1, 0, 1])
+            offset_y = random.choice([-1, 0, 1])
+            new_pos_x = attach_to_pos[0] + offset_x          
+            new_pos_y = attach_to_pos[1] + offset_y
+            while offset_x == 0 and offset_y == 0 or ([new_pos_x, new_pos_y] in self.room_set_pos):
+                offset_x = random.choice([-1, 0, 1])
+                offset_y = random.choice([-1, 0, 1])
+                new_pos_x = attach_to_pos[0] + offset_x          
+                new_pos_y = attach_to_pos[1] + offset_y
+            new_room = Room(room_name, attach_to_pos[0] + offset_x, attach_to_pos[1] + offset_y)
             self.room_set.add(new_room)
+            self.room_set_pos.append([new_pos_x, new_pos_y])
             new_room.add_neighbors()
 
 class Room:
     # initialize rooms
-    def __init__(self, name):
+    def __init__(self, name, pos_x, pos_y):
         self.name = name
+        self.pos = [pos_x, pos_y]
         self.inventory = []
         self.neighbors = {}
+        
+    def __str__(self):
+        return "%s room with the following neighbors:\n%r" % (self.name, self.neighbors)
 
     # called by map during initialization of game
     # build a dictionary attribute that has {other_room_object: 'north'}
     # then call other_room_object.neighbors and add to its dictionary
     # {this_room_object: 'south'}
     def add_neighbors(self):
-        for i in random.randrange(3):
-            room_names.pop()
-            new_room = Room(room_name)
-            j = random.randrange(9)
-            while cardinals[j] in self.neighbors.values():
-                j = random.randrange(9)
-            self.neighbors[new_room] = cardinals[j]
-            new_room.neighbors[self] = opposing[j]
+        global my_map
+        for i in my_map.room_set:
+            if i == self:
+                continue
+            neighbor_dir = vector_to_dir(self.get_pos(), i.get_pos())
+            if neighbor_dir != None:
+                self.neighbors[i] = neighbor_dir
+                i.neighbors[self] = opposing[cardinals.index(neighbor_dir)]
+        
+        #for i in random.randrange(3):
+        #    room_names.pop()
+        #    new_room = Room(room_name)
+        #    j = random.randrange(9)
+        #    while cardinals[j] in self.neighbors.values():
+        #        j = random.randrange(9)
+        #    self.neighbors[new_room] = cardinals[j]
+        #    new_room.neighbors[self] = opposing[j]
 
+    # return true if there is an empty space for another neighbor
+    def check_neighbors(self):
+        if len(self.neighbors) < 8:
+            return True
+        else:
+            return False
+            
+    def get_pos(self):
+        return self.pos
     # there is one active room at a time (this is the room the player is in).
     # active room dictates what can be interacted with and where player can go
     def activate(self):
@@ -96,6 +141,7 @@ class Room:
     # opposite of the activate method
     def deactivate(self):
         pass
+
 
 class Player:
     # initialize player. New player if new game; load player if load game
